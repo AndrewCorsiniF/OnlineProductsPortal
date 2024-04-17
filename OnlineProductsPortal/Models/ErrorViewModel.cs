8b6e71df274a29b563044f54a60760a8898a041e
Andrew Corsini <andrew_corsini_94@outlook.com>
2024-04-17 14:19:13 +0530

    Initial commit

---
+++ OnlineProductsPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineProductsPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
